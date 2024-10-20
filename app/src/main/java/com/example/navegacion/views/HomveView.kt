07ab7a306e73b9e56e19ef456aef69055161b47f
scarl <lunasecena@gmail.com>
2024-10-19 20:07:18 -0600

    Se realizaron los cambios de la tarea

--- app/src/main/java/com/example/navegacion/views/HomveView.kt
+++ app/src/main/java/com/example/navegacion/views/HomveView.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import com.example.navegacion.components.ActionButton
 import com.example.navegacion.components.MainButton
